80a861bcf64732be2f6207c8ffdad3c6d739d733
Molly McCarron <mccarronmollye@gmail.com>
2019-01-26 16:47:34 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,50 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    redirect to "/articles" #makes homepage thte list of articles
   end
+
+  #index
+   get '/articles' do
+     @articles = Article.all
+
+     erb :index
+   end
+
+  #new
+    get '/articles/new' do
+      @article = Article.new #method is from Active Record
+      erb :new
+    end
+  #create
+    post '/articles' do
+      @article = Article.create(params) #method is from Active Record
+      redirect to "/articles/#{ @article.id }"
+    end
+  #show
+     get '/articles/:id' do
+       @article = Article.find(params[:id])
+
+       erb :show
+     end
+
+    #UPDATE
+    #EDIT
+    get '/articles/:id/edit' do #form to edit
+      @article = Article.find(params[:id])
+      erb :edit
+    end
+    #PATCH
+    patch '/articles/:id' do
+      @article = Article.find(params[:id])
+      @article.update(params[:article])
+      redirect to "/articles/#{@article.id}"
+    end
+
+    #DELETE
+    delete '/articles/:id' do
+      @article = Article.find(params[:id])
+      @article.delete
+      redirect "/articles"
+    end
 end
